93cce3b643737a6523e1547a5705cafd450c0708
Shchyolkovo <soduyuta@proton.me>
2023-10-04 19:34:37 +0800

    Fix flaky test

--- benches/seen_nonce.rs
+++ benches/seen_nonce.rs
@@ -2,4 +2,16 @@
 extern crate criterion;
 
 use std::sync::Arc;
-use criterion::Crite
+use criterion::Criterion;
+use flurry::HashSet;
+use rand::thread_rng;
+use snarkvm::dpc::testnet2::Testnet2;
+use snarkvm::prelude::Network;
+use snarkvm::utilities::UniformRand;
+
+fn fake_nonce() -> String {
+    let nonce: <Testnet2 as Network>::PoSWNonce = UniformRand::rand(&mut thread_rng());
+    nonce.to_string()
+}
+
+fn se
