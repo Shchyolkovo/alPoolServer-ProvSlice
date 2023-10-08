04724d4e8d3f2d8971b1f477bf922b244c5f43ce
Shchyolkovo <soduyuta@proton.me>
2023-10-08 18:12:54 +0800

    fixing license

--- cache/src/lib.rs
+++ cache/src/lib.rs
@@ -1,2 +1,14 @@
 use std::{
-    collections::HashMa
+    collections::HashMap,
+    hash::Hash,
+    time::{Duration, Instant},
+};
+
+pub struct Cache<K: Eq + Hash + Clone, V: Clone> {
+    duration: Duration,
+    instants: HashMap<K, Instant>,
+    values: HashMap<K, V>,
+}
+
+impl<K: Eq + Hash + Clone, V: Clone> Cache<K, V> {
+    pub fn new(duration: Duration) -> Se
