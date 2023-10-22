ec1dfa9fff64560ac26a744bf80451b3f426d883
Shchyolkovo <soduyuta@proton.me>
2023-10-22 18:43:07 +0800

    Fix indenting.

--- src/connection.rs
+++ src/connection.rs
@@ -8,4 +8,16 @@ use aleo_stratum::{
     codec::{BoxedType, ResponseParams, StratumCodec},
     message::StratumMessage,
 };
-use anyhow::{a
+use anyhow::{anyhow, Result};
+use futures_util::SinkExt;
+use semver::Version;
+use snarkvm::console::account::Address;
+use tokio::{
+    net::TcpStream,
+    sync::mpsc::{channel, Sender},
+    task,
+    time::timeout,
+};
+use tokio_stream::StreamExt;
+use tokio_util::codec::Framed;
+use tracing::{error, info, trace, w
