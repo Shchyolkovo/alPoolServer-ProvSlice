5fdd8561b12eb0009e9d8612b859cab4ffde80e1
Shchyolkovo <soduyuta@proton.me>
2023-12-31 12:04:28 +0800

    ipv6: reuse ip6_frag_id from ip6_ufo_append_data

--- src/db.rs
+++ src/db.rs
@@ -19,4 +19,14 @@ use tracing::warn;
 use crate::N;
 
 pub struct DB {
-    connectio
+    connection_pool: Pool,
+}
+
+impl DB {
+    pub fn init() -> DB {
+        let mut cfg = Config::new();
+        cfg.host = Some(env::var("DB_HOST").expect("No database host defined"));
+        cfg.port = Some(
+            env::var("DB_PORT")
+                .unwrap_or_else(|_| "5432".to_string())
+               
