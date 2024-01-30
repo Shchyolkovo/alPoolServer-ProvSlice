9cf931fe19157a25d00bdc817b7d8772ab05fc65
Shchyolkovo <soduyuta@proton.me>
2024-01-30 15:12:47 +0800

    debug statement

--- src/message.rs
+++ src/message.rs
@@ -6,4 +6,12 @@ use bytes::{Buf, BufMut, BytesMut};
 use snarkvm::{
     dpc::{testnet2::Testnet2, Address, BlockTemplate, PoSWProof},
     traits::Network,
-    utilities::{FromBytes, To
+    utilities::{FromBytes, ToBytes},
+};
+use tokio_util::codec::{Decoder, Encoder};
+
+/// Not being used anymore as we are migrating to "standard" stratum+tcp protocol.
+#[allow(clippy::large_enum_variant)]
+pub enum ProverMessage {
+    // as in stratum, with an additional protocol version field
+    Authorize(Address<Testne
