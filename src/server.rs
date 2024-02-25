ea2a8c0cc940fac087d09984fa6b13d00d8c8b82
Shchyolkovo <soduyuta@proton.me>
2024-02-25 21:16:41 +0800

    dependency injection

--- src/server.rs
+++ src/server.rs
@@ -3,4 +3,15 @@ use std::{
     fmt::{Display, Formatter},
     net::SocketAddr,
     sync::{
-        at
+        atomic::{AtomicU32, AtomicU64, Ordering},
+        Arc,
+    },
+    time::{Duration, Instant},
+};
+
+use aleo_stratum::{codec::ResponseParams, message::StratumMessage};
+use flurry::HashSet as FlurryHashSet;
+use json_rpc_types::{Error, ErrorCode, Id};
+use snarkos_node_router_messages::UnconfirmedSolution;
+use snarkvm::{
+    c
