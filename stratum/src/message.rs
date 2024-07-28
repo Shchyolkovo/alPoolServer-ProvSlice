db05886a7c74e258b882f4e04fc48d02a44093b6
Shchyolkovo <soduyuta@proton.me>
2024-07-28 20:55:58 +0800

    Update lint

--- stratum/src/message.rs
+++ stratum/src/message.rs
@@ -1,1 +1,13 @@
-use json_r
+use json_rpc_types::{Error, Id};
+
+use crate::codec::ResponseParams;
+
+pub enum StratumMessage {
+    /// This first version doesn't support vhosts.
+    /// (id, user_agent, protocol_version, session_id)
+    Subscribe(Id, String, String, Option<String>),
+
+    /// (id, worker_name, worker_password)
+    Authorize(Id, String, String),
+
+    /// This is the di
